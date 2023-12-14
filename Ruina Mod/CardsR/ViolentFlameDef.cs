bc316593e3d1b517c523f6cf05f0dbdf98380390
gluee003 <lzhlzhlzh123@gmail.com>
2023-12-14 22:11:34 +0800

    implemented BurnMore status and ability

---
+++ Ruina Mod/CardsR/ViolentFlameDef.cs
@@ -0,0 +1,129 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLEntitySideloader.Entities;
+using LBoLEntitySideloader.Resource;
+using Mono.Cecil;
+using Ruina_Mod.CardsBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Ruina_Mod.BepinexPlugin;
+using LBoL.Core.Battle.BattleActions;
+using Ruina_Mod.Status;
+using LBoL.EntityLib.StatusEffects.Others;
+using LBoL.Core.StatusEffects;
+using System.Linq;
+using LBoL.Core.Units;
+
+namespace Ruina_Mod.CardsC
+{
+    public sealed class ViolentFlameDef : CardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ViolentFlame);
+        }
+
+        public override CardImages LoadCardImages()
+        {
+            var imgs = new CardImages(embeddedSource);
+            imgs.AutoLoad(this, extension: ".png");
+            return imgs;
+        }
+
+        public override LocalizationOption LoadLocalization()
+        {
+            var loc = new GlobalLocalization(embeddedSource);
+            loc.LocalizationFiles.AddLocaleFile(LBoL.Core.Locale.En, "CardsEn.yaml");
+            return loc;
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = new CardConfig(
+              Index: BepinexPlugin.sequenceTable.Next(typeof(CardConfig)),
+              Id: "",
+              Order: 10,
+              AutoPerform: true,
+              Perform: new string[0][],
+              GunName: "Simple1",
+              GunNameBurst: "Simple2",
+              DebugLevel: 0,
+              Revealable: false,
+              IsPooled: true,
+              HideMesuem: false,
+              IsUpgradable: true,
+              Rarity: Rarity.Common,
+              Type: CardType.Ability,
+              TargetType: TargetType.Nobody,
+              Colors: new List<ManaColor>() { ManaColor.Red },
+              IsXCost: false,
+              Cost: new ManaGroup() { Red = 1, Any = 1 },
+              UpgradedCost: new ManaGroup() { Any = 2 },
+              MoneyCost: null,
+              Damage: null,
+              UpgradedDamage: null,
+              Block: null,
+              UpgradedBlock: null,
+              Shield: null,
+              UpgradedShield: null,
+              Value1: 2,
+              UpgradedValue1: null,
+              Value2: null,
+              UpgradedValue2: null,
+              Mana: null,
+              UpgradedMana: null,
+              Scry: null,
+              UpgradedScry: null,
+
+              ToolPlayableTimes: null,
+
+              Loyalty: null,
+              UpgradedLoyalty: null,
+              PassiveCost: null,
+              UpgradedPassiveCost: null,
+              ActiveCost: null,
+              UpgradedActiveCost: null,
+              UltimateCost: null,
+              UpgradedUltimateCost: null,
+
+              Keywords: Keyword.None,
+              UpgradedKeywords: Keyword.Initial,
+              EmptyDescription: false,
+              RelativeKeyword: Keyword.None,
+              UpgradedRelativeKeyword: Keyword.None,
+
+              RelativeEffects: new List<string>() { "BurnStatus" },
+              UpgradedRelativeEffects: new List<string>() { "BurnStatus" },
+              RelativeCards: new List<string>() { },
+              UpgradedRelativeCards: new List<string>() { },
+
+              Owner: "Roland",
+              ImageId: "",
+              UpgradeImageId: "",
+
+              Unfinished: false,
+              Illustrator: "Nai_ Ga",
+              SubIllustrator: new List<string>() { }
+           );
+
+
+            return cardConfig;
+        }
+    }
+    [EntityLogic(typeof(ViolentFlameDef))]
+    public sealed class ViolentFlame : Card
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return base.BuffAction<BurnMoreStatus>(base.Value1, 0, 0, 0, 0.2f);
+            yield break;
+        }
+    }
+}
+
