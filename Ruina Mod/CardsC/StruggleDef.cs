f35e9904e211422d0fbcc29bed1d3be2175e8c84
gluee003 <lzhlzhlzh123@gmail.com>
2024-01-16 21:42:42 +0800

    implemented ProtectionStatus and defense card

---
+++ Ruina Mod/CardsC/StruggleDef.cs
@@ -0,0 +1,132 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLEntitySideloader.Entities;
+using LBoLEntitySideloader.Resource;
+using Mono.Cecil;
+using Ruina_Mod.CardsBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Ruina_Mod.BepinexPlugin;
+using LBoL.Core.Battle.BattleActions;
+using Ruina_Mod.Status;
+using LBoL.EntityLib.StatusEffects.Others;
+using LBoL.Core.StatusEffects;
+using System.Linq;
+using LBoL.Core.Units;
+using static UnityEngine.UI.CanvasScaler;
+
+namespace Ruina_Mod.CardsC
+{
+    public sealed class StruggleDef : CardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(Struggle);
+        }
+
+        public override CardImages LoadCardImages()
+        {
+            var imgs = new CardImages(embeddedSource);
+            imgs.AutoLoad(this, extension: ".png");
+            return imgs;
+        }
+
+        public override LocalizationOption LoadLocalization()
+        {
+            var loc = new GlobalLocalization(embeddedSource);
+            loc.LocalizationFiles.AddLocaleFile(LBoL.Core.Locale.En, "CardsEn.yaml");
+            return loc;
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = new CardConfig(
+              Index: BepinexPlugin.sequenceTable.Next(typeof(CardConfig)),
+              Id: "",
+              Order: 10,
+              AutoPerform: true,
+              Perform: new string[0][],
+              GunName: "Simple1",
+              GunNameBurst: "Simple2",
+              DebugLevel: 0,
+              Revealable: false,
+              IsPooled: true,
+              HideMesuem: false,
+              IsUpgradable: true,
+              Rarity: Rarity.Uncommon,
+              Type: CardType.Defense,
+              TargetType: TargetType.Nobody,
+              Colors: new List<ManaColor>() { ManaColor.Colorless },
+              IsXCost: false,
+              Cost: new ManaGroup() { Any = 3 },
+              UpgradedCost: null,
+              MoneyCost: null,
+              Damage: null,
+              UpgradedDamage: null,
+              Block: 8,
+              UpgradedBlock: 10,
+              Shield: null,
+              UpgradedShield: null,
+              Value1: 2,
+              UpgradedValue1: null,
+              Value2: 3,
+              UpgradedValue2: 4,
+              Mana: null,
+              UpgradedMana: null,
+              Scry: null,
+              UpgradedScry: null,
+
+              ToolPlayableTimes: null,
+
+              Loyalty: null,
+              UpgradedLoyalty: null,
+              PassiveCost: null,
+              UpgradedPassiveCost: null,
+              ActiveCost: null,
+              UpgradedActiveCost: null,
+              UltimateCost: null,
+              UpgradedUltimateCost: null,
+
+              Keywords: Keyword.None,
+              UpgradedKeywords: Keyword.None,
+              EmptyDescription: false,
+              RelativeKeyword: Keyword.None,
+              UpgradedRelativeKeyword: Keyword.None,
+
+              RelativeEffects: new List<string>() { "ProtectionStatus" },
+              UpgradedRelativeEffects: new List<string>() { "ProtectionStatus" },
+              RelativeCards: new List<string>() { },
+              UpgradedRelativeCards: new List<string>() { },
+
+              Owner: "Roland",
+              ImageId: "",
+              UpgradeImageId: "",
+
+              Unfinished: false,
+              Illustrator: "Nai_ Ga",
+              SubIllustrator: new List<string>() { }
+           );
+
+
+            return cardConfig;
+        }
+    }
+    [EntityLogic(typeof(StruggleDef))]
+    public sealed class Struggle : RuinaCard
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return base.DefenseAction();
+            yield return base.DefenseAction();
+            yield return base.BuffAction<ProtectionStatus>(base.Value2);
+            yield break;
+        }
+    }
+}
+
---
+++ Ruina Mod/Status/ProtectionStatus.cs
@@ -0,0 +1,109 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.StatusEffects;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLEntitySideloader.Entities;
+using LBoLEntitySideloader.Resource;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static Ruina_Mod.BepinexPlugin;
+using LBoL.Core.Units;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoL.Base.Extensions;
+using System.Linq;
+using static Ruina_Mod.RuinaCard;
+using LBoL.EntityLib.StatusEffects.Basic;
+
+namespace Ruina_Mod.Status
+{
+    public sealed class ProtectionEffect : StatusEffectTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(ProtectionStatus);
+        }
+
+        public override LocalizationOption LoadLocalization()
+        {
+            var loc = new GlobalLocalization(embeddedSource);
+            loc.LocalizationFiles.AddLocaleFile(LBoL.Core.Locale.En, "StatusEffectsEn.yaml");
+            return loc;
+        }
+
+        public override Sprite LoadSprite()
+        {
+            return ResourceLoader.LoadSprite("ProtectionSE.png", BepinexPlugin.embeddedSource);
+        }
+
+        public override StatusEffectConfig MakeConfig()
+        {
+            {
+                var statusEffectConfig = new StatusEffectConfig(
+                                Id: "",
+                                Index: 0,
+                                Order: 10,
+                                Type: StatusEffectType.Positive,
+                                IsVerbose: false,
+                                IsStackable: true,
+                                StackActionTriggerLevel: null,
+                                HasLevel: true,
+                                LevelStackType: StackType.Add,
+                                HasDuration: false,
+                                DurationStackType: StackType.Add,
+                                DurationDecreaseTiming: DurationDecreaseTiming.TurnStart,
+                                HasCount: false,
+                                CountStackType: StackType.Keep,
+                                LimitStackType: StackType.Keep,
+                                ShowPlusByLimit: false,
+                                Keywords: Keyword.None,
+                                RelativeEffects: new List<string>() { },
+                                VFX: "Default",
+                                VFXloop: "Default",
+                                SFX: "Default"
+                    );
+                return statusEffectConfig;
+            }
+        }
+    }
+    [EntityLogic(typeof(ProtectionEffect))]
+    public sealed class ProtectionStatus : StatusEffect
+    {
+        protected override void OnAdded(Unit unit)
+        {
+            base.HandleOwnerEvent<DamageEventArgs>(unit.DamageReceiving, new GameEventHandler<DamageEventArgs>(this.OnDamageReceiving));
+            base.ReactOwnerEvent<UnitEventArgs>(base.Owner.TurnStarting, new EventSequencedReactor<UnitEventArgs>(this.OnOwnerTurnStarting));
+        }
+        private void OnDamageReceiving(DamageEventArgs args)
+        {
+            DamageInfo damageInfo = args.DamageInfo;
+            if (damageInfo.DamageType == DamageType.Attack)
+            {
+                args.DamageInfo = damageInfo.ReduceBy(base.Level);
+                args.AddModifier(this);
+            }
+        }
+        private IEnumerable<BattleAction> OnOwnerTurnStarting(UnitEventArgs args)
+        {
+            if (base.IsAutoDecreasing)
+            {
+                base.Level--;
+                if (base.Level == 0)
+                {
+                    yield return new RemoveStatusEffectAction(this, true, 0.1f);
+                }
+            }
+            else
+            {
+                base.IsAutoDecreasing = true;
+            }
+        }
+    }
+}
+
