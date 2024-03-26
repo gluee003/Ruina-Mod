6246719fc1f7bde1cf74bbe2c7515a82d8a68768
gluee003 <lzhlzhlzh123@gmail.com>
2024-03-26 23:19:47 +0800

    refactored AttackType as bit flags, cards can have multiple attack types now. Overrode RuinaCard.Description to add page_range and attack_type "keywords" to cards

--- Ruina Mod/RuinaCard.cs
+++ Ruina Mod/RuinaCard.cs
@@ -1,18 +1,26 @@
-﻿using LBoL.Core.Cards;
+﻿using LBoL.Base.Extensions;
+using LBoL.Core;
+using LBoL.Core.Cards;
+using System;
 
 
 namespace Ruina_Mod
 {
     public abstract class RuinaCard : Card
     {
+        [Flags]
         public enum AttackType
         {
-            None,
-            Slash,
-            Pierce,
-            Blunt
+            None = 0b000,
+            Slash = 0b001,
+            Pierce = 0b010,
+            Blunt = 0b100
         }
         public virtual AttackType attackType{ get; }
+        public string attack_type
+        {
+            get { return attackType.ToString().Replace(",", "") != "None" ? attackType.ToString().Replace(",", "") : ""; }
+        }
         public enum PageType
         {
             Normal,
@@ -27,5 +35,23 @@ namespace Ruina_Mod
             Range
         }
         public virtual PageRange pageRange { get;  }
+        public string page_range
+        {
+            get { return pageRange.ToString() != "None" ? pageRange.ToString() : ""; }
+        }
+        public override string Description
+        {
+            get
+            {
+                if (!page_range.IsNullOrEmpty() || !attack_type.IsNullOrEmpty())
+                {
+                    return StringDecorator.Decorate(base.Description + $"\n|{page_range} {attack_type}|");
+                }
+                else
+                {
+                    return base.Description;
+                }
+            }
+        }
     }
 }
